ebf7d31afd4c5378751d57fa68c6fb088b9fa9b9
Elise Kain <elise.kain@gmail.com>
2015-12-16 15:58:00 -0500

    Complete lab day 1

---
+++ js/app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function(){
+  angular
+  .module("wdi_radio", [
+    "ui.router",
+    "songs"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/songs/index.html",
+      controller: "SongIndexController",
+      controllerAs: "SongIndexViewModel"
+    })
+    .state("welcome", {
+      url: "/",
+      templateUrl: "js/welcome.html"
+    })
+    .state("songNew", {
+      url: "/songs/new",
+      templateUrl: "js/songs/new.html",
+      controller: "SongNewController",
+      controllerAs: "SongNewViewModel"
+    })
+    .state("songShow", {
+      url: "/songs/:id",
+      templateUrl: "js/songs/show.html",
+      controller: "SongShowController",
+      controllerAs: "SongShowViewModel"
+    })
+  }
+})();
