b32401992f029fedc0081491c51cdb94a48e3635
Britton Walker <bwalker1801@gmail.com>
2015-12-16 16:04:35 -0500

    create new song and set up edit controller

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
 "use strict";
 
-(function(){
+(function() {
   angular
-  .module("radioLab", [
-    "ui.router",
-    "songs"
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ]);
+    .module("radioLab", [
+      "ui.router",
+      "songs"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ]);
 
-  function RouterFunction($stateProvider){
+  function RouterFunction($stateProvider) {
     $stateProvider
-    .state("songsIndex", {
-      url: "/songs",
-      templateUrl: "js/songs/index.html",
-      controller: "SongIndexController",
-      controllerAs: "SongIndexViewModel"
-    })
-    .state("songShow", {
-      url: "/songs/:id",
-      templateUrl: "js/songs/show.html",
-      controller: "SongShowController",
-      controllerAs: "SongShowViewModel"
-    });
+      .state("songsIndex", {
+        url: "/songs",
+        templateUrl: "js/songs/index.html",
+        controller: "SongIndexController",
+        controllerAs: "SongIndexViewModel"
+      })
+      .state("songNew", {
+        url: "/songs/new",
+        templateUrl: "js/songs/new.html",
+        controller: "SongNewController",
+        controllerAs: "SongNewViewModel"
+      })
+      .state("songShow", {
+        url: "/songs/:id",
+        templateUrl: "js/songs/show.html",
+        controller: "SongShowController",
+        controllerAs: "SongShowViewModel"
+      })
+      .state("songEdit", {
+        url: "/songs/:id/edit",
+        templateUrl: "js/songs/edit.html",
+        controller: "SongEditController",
+        controllerAs: "SongEditViewModel"
+      });
   }
 }());
---
+++ js/songs/show.controller.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function(){
+  angular
+    .module( "songs" )
+    .controller( "SongShowController", [
+      "SongFactory",
+      "$stateParams",
+      SongShowControllerFunction
+    ]);
+
+  function SongShowControllerFunction( SongFactory, $stateParams ){
+    this.song = SongFactory.get({id: $stateParams.id});
+  }
+}());
