d448481cf2f095b0f6e5ff783010e5655db233a8
Chase Gruber <chasegruber@gmail.com>
2015-12-16 16:48:41 -0500

    U in crUd

--- js/songs/show.controller.js
+++ js/songs/show.controller.js
@@ -14,6 +14,9 @@
     this.delete = function(){
       SongFactory.remove({id: $stateParams.id});
     }
+    this.update = function(){
+      SongFactory.update({id: $stateParams.id},this.song);
+    }
     this.formVisible = false;
     this.toggleForm = function(){
       this.formVisible = (this.formVisible)?false:true;
