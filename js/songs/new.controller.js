fefe435a7ca93a36b353e3b8c74bfe6102d888de
Sarah Hersh <schersh@gmail.com>
2015-12-16 15:58:22 -0500

    created songs index and show views

---
+++ js/songs/new.controller.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function(){
+  angular
+  .module("songs")
+  .controller("SongNewController", [
+    "SongFactory",
+    SongNewControllerFunction
+  ]);
+
+  function SongNewControllerFunction(SongFactory){
+    this.song = new SongFactory();
+    this.create = function(){
+      this.song.$save();
+      ({ redirectTo: '/songs' });
+    }
+
+  }
+}());
---
+++ js/songs/song.factory.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function(){
+  angular
+  .module("songs")
+  .factory( "SongFactory", [
+    "$resource",
+    SongFactoryFunction
+  ]);
+  function SongFactoryFunction($resource){
+    return $resource("http://localhost:3000/songs/:id");
+  }
+}());
