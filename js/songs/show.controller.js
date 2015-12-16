20a3e88eb1c51f04301b5d54223665e6bb8c6933
Thomas Solow <thomassolow@gmail.com>
2015-12-16 16:15:19 -0500

    added edit

--- js/songs/show.controller.js
+++ js/songs/show.controller.js
@@ -11,11 +11,11 @@
 
   function SongShowControllerFunction(SongFactory, $stateParams){
     this.song = SongFactory.get({id: $stateParams.id})
+    this.edit = function(){
+      this.song.$update({id: $stateParams.id})
+    }
     this.delete = function(){
       this.song.$delete({id: $stateParams.id});
-    }
-    this.update = function(){
-      // edit goes here
     }
   }
 })();
