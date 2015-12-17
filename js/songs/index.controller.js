0939c26c7f1850dd0ce378865dd53ae41396ee10
Elise Kain <elise.kain@gmail.com>
2015-12-17 16:07:02 -0500

    Audio plays in show view

--- js/songs/index.controller.js
+++ js/songs/index.controller.js
@@ -5,10 +5,11 @@
   .module("songs")
   .controller("SongIndexController", [
     "SongFactory",
+    "$sce",
     SongIndexControllerFunction
   ]);
 
-  function SongIndexControllerFunction(SongFactory){
+  function SongIndexControllerFunction(SongFactory, $sce){
     this.songs = SongFactory.query();
   }
 })();
