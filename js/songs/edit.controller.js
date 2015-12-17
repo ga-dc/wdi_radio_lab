aaca9de96b71b6433bdb24128d304c403223fb88
Alice Frumin <alifrumin@yahoo.com>
2015-12-17 14:39:07 -0500

    show link working and edit and delete refresshing

--- js/songs/edit.controller.js
+++ js/songs/edit.controller.js
@@ -13,12 +13,16 @@
   function SongEditControllerFunction( SongFactory, $stateParams, $state ){
     this.song = SongFactory.get({id: $stateParams.id});
     this.update = function(){
-      this.song.$update({id: $stateParams.id})
-      $state.go("songsIndex")
+      this.song.$update({id: $stateParams.id}).then(function(){
+            $state.go("songsIndex")
+      })
+
     }
     this.destroy = function(){
-      this.song.$delete({id: $stateParams.id});
-      $state.go("songsIndex")
+      this.song.$delete({id: $stateParams.id}).then(function(){
+        $state.go("songsIndex")
+      });
+
     }
   }
 }());
