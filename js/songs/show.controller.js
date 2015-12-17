d5c1d38aea7a5015579c6b10e4cdd837f7d51d92
Chase Gruber <chasegruber@gmail.com>
2015-12-17 14:29:28 -0500

    remove unnecessary controllers

--- js/songs/show.controller.js
+++ js/songs/show.controller.js
@@ -11,15 +11,5 @@
 
   function SongShowControllerFunction(SongFactory,$stateParams){
     this.song = SongFactory.get({id: $stateParams.id});
-    this.delete = function(){
-      SongFactory.remove({id: $stateParams.id});
-    }
-    this.update = function(){
-      SongFactory.update({id: $stateParams.id},this.song);
-    }
-    this.formVisible = false;
-    this.toggleForm = function(){
-      this.formVisible = (this.formVisible)?false:true;
-    }
   }
 })()
