04eab89567b067e80fb83df251aa2c6a22292abc
Chase Gruber <chasegruber@gmail.com>
2015-12-17 16:07:58 -0500

    stuck on audio player

--- js/songs/index.controller.js
+++ js/songs/index.controller.js
@@ -4,13 +4,19 @@
   angular
   .module("songs")
   .controller("SongIndexController", [
+    "$sce",
     "SongFactory",
     SongIndexControllerFunction
   ]);
 
-  function SongIndexControllerFunction(SongFactory){
+  function SongIndexControllerFunction($sce,SongFactory){
     this.songs = SongFactory.query();
     this.newSong = new SongFactory();
+    this.play = function(song){
+      this.playSong = song
+      // workaround for ng-src not allowing expression {{song.audio_url}}
+      this.playSong.encodedUrl = $sce.trustAsResourceUrl(song.audio_url)
+    }
   }
 
 })();
