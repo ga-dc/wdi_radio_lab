e9aa10f22371da3d211793287c552596ea9851d1
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-12-17 16:37:26 -0500

    adds save, delete and new buttons functionality

--- js/songs/form.directive.js
+++ js/songs/form.directive.js
@@ -1,14 +1,34 @@
 (function(){
   angular
   .module('songs')
-  .directive('songForm', function(){
+  .directive('songForm', [
+    "$state",
+    "SongFactory",
+    function($state, Song){
     return {
       templateUrl: 'js/songs/_song_form.html',
       replace: true,
       scope: {
         song: '=',
         formType: '@'
+      },
+      link: function(scope){
+        scope.create = function(){
+          scope.song.$save(scope.song, function(song){
+            $state.go('songShow',song);
+          })
+        }
+        scope.update = function(){
+          scope.song.$update(scope.song, function(song){
+            $state.go('songShow',song);
+          })
+        }
+        scope.destroy = function(){
+          scope.song.$delete(scope.song, function(song){
+            $state.go('songIndex', song);
+          })
+        }
       }
     }
-  });
+  }]);
 })();
