d37b5aa496023f7158e477ef3f121d478681747e
Alice Frumin <alifrumin@yahoo.com>
2015-12-18 16:10:50 -0500

    Janky sign in working

--- js/users/sessions.controller.js
+++ js/users/sessions.controller.js
@@ -5,6 +5,17 @@
       this.signupForm = {};
       this.signinForm = {};
 
+      this.signup = function() {
+        $auth.submitRegistration(this.signupForm)
+          .then(function(resp) {
+            $state.go('songsIndex');
+          })
+          .catch(function(resp) {
+            console.log("Error!");
+            console.log(resp);
+          });
+      };
+
       this.signin = function() {
         $auth.submitLogin(this.signinForm)
           .then(function(resp) {
@@ -14,12 +25,13 @@
           .catch(function(resp) {
             console.log("Signin failure:", resp);
           });
-            };
-          $scope.$on('$stateChangeSuccess',
-            function(event, toState, toParams, fromState, fromParams) {
-              if (toState.name == 'signout') {
-                $auth.signOut();
-              }
-            });
+      };
+
+      $scope.$on('$stateChangeSuccess',
+        function(event, toState, toParams, fromState, fromParams) {
+          if (toState.name == 'signout') {
+            $auth.signOut();
+          }
+        });
     });
 })();
