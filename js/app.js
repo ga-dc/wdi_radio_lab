c9b66a91ba26727a7e6b4ac1b24aea45599d985a
Alice Frumin <alifrumin@yahoo.com>
2015-12-18 15:38:19 -0500

    added pages for sign in out and up

--- js/app.js
+++ js/app.js
@@ -53,6 +53,18 @@
         templateUrl: "js/users/signin.html",
         controller: "SessionsController",
         controllerAs: "SessionsViewModel"
+      })
+      .state("signup", {
+        url: "/signup",
+        templateUrl: "js/users/signup.html",
+        controller: "SessionsController",
+        controllerAs: "SessionsViewModel"
+      })
+      state("signout", {
+        url: "/signout",
+        templateUrl: "js/users/signout.html",
+        controller: "SessionsController",
+        controllerAs: "SessionsViewModel"
       });
   }
 }());
--- js/users/sessions.controller.js
+++ js/users/sessions.controller.js
@@ -2,6 +2,12 @@
   angular
   .module("radioLab")
   .controller("SessionsController", function($auth, $state, $scope){
+    $scope.$on('$stateChangeSuccess',
+            function(event, toState, toParams, fromState, fromParams){
+              if(toState.name == 'signout') {
+                $auth.signOut();
+              }
+            });
     this.signinForm = {};
     this.signin = function() {
       $auth.submitLogin(this.signinForm)
