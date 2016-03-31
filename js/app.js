7f07e349c47cbcd3bb027f1a78de087dd568dd68
winishasmith <winishasmith@gmail.com>
2016-03-31 13:57:59 -0400

    added router info for index and show

--- js/app.js
+++ js/app.js
@@ -3,6 +3,38 @@
 (function(){
   angular
   .module("radio", [
-    "ui.router"
+    "ui.router",
+    "songs"
   ])
-})();
+
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  .controller("indexCtrl", [
+    indexCtrlFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("index", {
+      url: "/",
+      templateUrl: "js/radio/index.html"
+      controller: "indexCtrl",
+      controllerAs: "indexVM"
+    })
+
+    .state("show", {
+      url: "/:id",
+    })
+
+  }
+
+  function indexCtrlFunction(){
+    var indexVM = this;
+    indexVM.hello = "hello world";
+  }
+
+
+}());
