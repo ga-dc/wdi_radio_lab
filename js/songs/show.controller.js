872bbb66d421a4cccc155b3c49c2b32918f1dcca
Melissa Miller <lis.d.miller@gmail.com>
2016-03-31 14:48:02 -0400

    Adds styling and plays music
    Additional styling to index view.
    
    Adds styling for show view
    
    Creates a playSong method and implements it on show view

--- js/songs/show.controller.js
+++ js/songs/show.controller.js
@@ -5,11 +5,15 @@
   .module('songs')
   .controller('SongShowController', [
     "SongFactory",
+    "$sce",
     "$stateParams",
     SongShowControllerFunction
   ]);
 
-  function SongShowControllerFunction(SongFactory, $stateParams) {
+  function SongShowControllerFunction(SongFactory, $sce, $stateParams) {
     this.song = SongFactory.get({id: $stateParams.id});
+    this.playMusic = function(song) {
+      return song.encodedurl = $sce.trustAsResourceUrl(this.song.audio_url);
+    }
   }
 })();
