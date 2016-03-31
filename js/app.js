edcc2d7a79a99db34e4969478edfdbd37dd49850
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-31 16:19:18 -0400

    cleanup showCtrl

--- js/app.js
+++ js/app.js
@@ -6,33 +6,48 @@
   ])
   .config([
     "$stateProvider",
-      function($stateProvider) {
-        $stateProvider
-        .state("index", {
-          url: "/",
-          templateUrl: "songs.index.html"
-        })
-        .state("show", {
-          url: "/:id"
-        });
-      }
+    function($stateProvider) {
+      $stateProvider
+      .state("index", {
+        url: "/",
+        templateUrl: "songs.index.html"
+      })
+      .state("show", {
+        url: "/:id",
+        template: "<song-info data-song='vm.song'></song-info>",
+        controller: "showCtrl",
+        controllerAs: "vm"
+      });
+    }
   ])
-  .controller("songsCtrl", [
+  .controller("indexCtrl", [
     "Song",
     function(Song){
       var vm = this;
       vm.songs = Song.all;
     }
   ])
+  .controller("showCtrl", [
+    "Song",
+    "$stateParams",
+    function (Song, $stateParams){
+      var vm = this;
+        Song.all.forEach(function(song){
+          if(song.id == $stateParams.id){
+            vm.song = song;
+          }
+        });
+    }
+  ])
   .factory("Song", [
     "$resource",
     function($resource){
       var Song = $resource("http://localhost:3000/songs.json", {}, {
-         update: {method: "PUT"}
-       });
-       Song.all = Song.query();
-       return Song;
-     }
+        update: {method: "PUT"}
+      });
+      Song.all = Song.query();
+      return Song;
+    }
   ])
   .directive("songInfo", [
     "Song",
