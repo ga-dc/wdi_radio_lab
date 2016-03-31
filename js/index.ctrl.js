dc7a88bed8008e26325b3c07123f25507150dd57
Ryan Krolick <rkrolick@gmail.com>
2016-03-31 16:37:29 -0400

    Trying to get player to work

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -4,12 +4,30 @@
   angular
   .module("radio")
   .controller("RadioIndexCtrl", [
+    //"$sce",
     "SongFactory",
     ctrlFunc
   ]);
 
   function ctrlFunc(SongFactory){
     this.songs = SongFactory.query();
-    console.log("BONER!!!");
+    this.showPlayer = false;
+    this.playerSong = null;
+
+    this.togglePlayer = function(songId){
+      if (this.showPlayer){this.showPlayer=false;}
+      else {
+        this.showPlayer = true;
+        this.playerSong = SongFactory.get({id: songId});
+        //.$promise.then(function(song){console.log(song.title);})
+        //NOTE: This will allow use to call playersong correctly directly after the assignment. Needed because of async js on .get
+      }
+    }
+
+    this.getPlayerUrl = function(){
+      return this.playerSong; //TODO: THIS WILL WORK.
+      // return this.playerSong.audio_url; //TODO: THIS WILL WORK BUT THROW ERRORS.
+      //$sce.trustAsResourceUrl("http://www.google.com");
+    }
   }
 })();
