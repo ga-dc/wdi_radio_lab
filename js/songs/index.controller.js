f07ecc76bac1c7abfacd41749cf640daa4a9f10a
Melissa Miller <lis.d.miller@gmail.com>
2016-03-31 11:08:49 -0400

    Fixes issue with songs not showing up on index

--- js/songs/index.controller.js
+++ js/songs/index.controller.js
@@ -9,6 +9,12 @@
   ]);
 
   function SongIndexControllerFunction($state) {
+    this.songs = [
+      {title: 'this'},
+      {title: 'is'},
+      {title: 'a'},
+      {title: 'song'}
+    ]
     this.goToIndex = function() {
       $state.go('songIndex')
     }
