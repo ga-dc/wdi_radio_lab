25d6534c039fda09c68ee3c342de74dfb2fa9131
Charles <charlesthwang@gmail.com>
2016-03-31 15:49:34 -0400

    added delete method and button

--- js/songs/edit.controller.js
+++ js/songs/edit.controller.js
@@ -13,6 +13,9 @@
     this.song = SongFactory.get({id: $stateParams.id});
     this.update = function(){
       this.song.$update({id: $stateParams.id})
-    }
-  }
+    };
+    this.destroy = function(){
+      this.song.$delete({id: $stateParams.id})
+    };
+  };
 })();
