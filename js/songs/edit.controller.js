559d85ab593f2c68684575bfa142416265bdbf22
Christine Movius <christine.movius@gmail.com>
2016-03-31 14:58:38 -0400

    Edit and delete works now. Didn't use directive. Would like to use directive in the future.

--- js/songs/edit.controller.js
+++ js/songs/edit.controller.js
@@ -6,13 +6,15 @@
     .controller( "songEditController", [
       "songFactory",
       "$stateParams",
+      "$state",
       songEditControllerFunction
     ]);
 
-  function songEditControllerFunction( songFactory, $stateParams ){
+  function songEditControllerFunction( songFactory, $stateParams, $state ){
     this.song = songFactory.get({id: $stateParams.id});
     this.update = function(){
-      this.song.$update({id: $stateParams.id})
+      this.song.$update({id: $stateParams.id});
+      // $state.go("songIndex"); This "works" but you have to refresh the page to get the changes grrr
     }
     this.destroy = function(){
       this.song.$delete({id: $stateParams.id});
