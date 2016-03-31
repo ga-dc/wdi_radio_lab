a3c2be1503d34c9d5d0384b8370c2fe84329d2d5
Hilary Masland <hilarymasland@gmail.com>
2016-03-31 14:40:35 -0400

    songs showing up

--- js/songs/song.factory.js
+++ js/songs/song.factory.js
@@ -11,7 +11,7 @@
   function FactoryFunction($resource){
 
     return $resource (
-      "http://localhost:3000/songs", {}, {
+      "http://localhost:3000/songs/:id", {}, {
       update: {method: "PUT"}
     });
   }
