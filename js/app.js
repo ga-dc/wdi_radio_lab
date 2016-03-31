962307f80d0c5f08b01e05d5ffd52e1fd5de1654
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-31 10:45:11 -0400

    made it a one folder app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 "use strict";
 
 (function(){
+  angular
+  .module( "radio", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state
+  }
 
 }());
