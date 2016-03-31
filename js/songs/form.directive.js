cb82c243eb7e5de53a5f8bc8e75c78b05081584b
Patrick Moran <patrick_moran@mac.com>
2016-03-31 16:21:47 -0400

    update and delete using custom directive

--- js/songs/form.directive.js
+++ js/songs/form.directive.js
@@ -2,12 +2,24 @@
 
 (function() {
   angular.module("songs")
-    .directive("songForm", function() {
+    .directive("songForm", function($state) {
       return {
         templateUrl: "js/songs/_song_form.html",
+        transclude: true,
         scope: {
-          vm: "=",
-          formType: "@"
+          song: "="
+        },
+        link: function(scope) {
+          scope.edit = function() {
+            scope.song.$update({id: scope.song.id}).then(function() {
+              $state.go("songsIndex", {}, {reload: true});
+            });
+          }
+          scope.delete = function() {
+            scope.song.$delete({id: scope.song.id}).then(function() {
+              $state.go("songsIndex", {}, {reload: true});
+            });
+          }
         }
       }
     });
