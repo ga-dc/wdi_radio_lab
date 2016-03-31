a5039a7108967cfbc659845eb9e03fe703074b6f
Charles <charlesthwang@gmail.com>
2016-03-31 16:30:13 -0400

    added redirect to index view upon 'update' and delete'

--- js/songs/edit.controller.js
+++ js/songs/edit.controller.js
@@ -6,16 +6,20 @@
   .controller("SongEditController", [
     "SongFactory",
     "$stateParams",
+    "$state",
     SongEditController
   ]);
 
-  function SongEditController(SongFactory, $stateParams){
+  function SongEditController(SongFactory, $stateParams, $state){
     this.song = SongFactory.get({id: $stateParams.id});
     this.update = function(){
       this.song.$update({id: $stateParams.id})
     };
     this.destroy = function(){
       this.song.$delete({id: $stateParams.id})
+    };
+    this.goToIndex = function(){
+      $state.go("songIndex")
     };
   };
 })();
