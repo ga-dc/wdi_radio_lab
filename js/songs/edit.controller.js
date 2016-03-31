7abf9564ec85d90d47ab31d38890cdc66c7ad279
Melissa Miller <lis.d.miller@gmail.com>
2016-03-31 16:28:32 -0400

    Adds delete function to edit controller and view

--- js/songs/edit.controller.js
+++ js/songs/edit.controller.js
@@ -12,7 +12,10 @@
   function SongEditControllerFunction(SongFactory, $stateParams) {
     this.song = SongFactory.get({id: $stateParams.id});
     this.update = function() {
-      this.song.$update({id: $stateParams.id})
+      this.song.$update({id: $stateParams.id});
     }// end update function
+    this.destroy = function() {
+      this.song.$delete({id: $stateParams.id});
+    } // end destroy function
   } // end edit song controller function
 })();
