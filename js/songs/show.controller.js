e7923402ec983d713a48b27986d6417c610bea35
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-31 12:35:50 -0400

    lunch break time

---
+++ js/songs/show.controller.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function(){
+  angular
+  .module( "songs" )
+  .controller( "SongShowController", [
+    "SongFactory",
+    "$stateParams",
+    SongShowControllerFunction
+  ]);
+  function SongShowControllerFunction(SongFactory, $stateParams){
+    this.songs = SongFactory.get({id: $stateParams.id});
+  }
+})
