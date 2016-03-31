e0ef49960f9d9e4398dbe10f25054905332cd33d
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-31 12:02:44 -0400

    show & index controller added

--- js/wadio/show.controller.js
+++ js/wadio/show.controller.js
@@ -1,6 +1,15 @@
 "use strict";
 
 (function(){
+  angular
+  .module("WDIRadio")
+  .controller("WadioShowController",[
+    "WadioFactory",
+    "$stateParams",
+    WadioShowControllerFunction
+  ]);
 
-}
+  function WadioShowControllerFunction(WadioFactory, $stateParams){
+    this.wadio = WadioFactory.get({:id $stateParams.id});
+  }
 }());
