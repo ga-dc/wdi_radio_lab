c929c330cd864251aa2af12f0d05d39a8909a04f
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-31 12:56:52 -0400

    adds show page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 (function(){
@@ -10,17 +9,60 @@
   .config([
     "$stateProvider",
     RouterFunction
+  ])
+  .factory("Song", [
+    "$resource",
+    songFactoryFunction
+  ])
+  .controller("indexCtrl", [
+    "Song",
+    indexCtrlFunction
+  ])
+  .controller("showCtrl", [
+    "Song",
+    "$stateParams",
+    showCtrlFunction
   ]);
 
   function RouterFunction($stateProvider){
     $stateProvider
     .state("index", {
       url: "/",
-      templateUrl: "songs.index.html"
+      templateUrl: "songs.index.html",
+      controller: "indexCtrl",
+      controllerAs: "indexVM"
     })
     .state("show", {
-      url: "/:id"
+      url: "/:id",
+      templateUrl: "songs.show.html",
+      controller: "showCtrl",
+      controllerAs: "showVM"
     });
   }
+
+  function songFactoryFunction($resource){
+    var Song = $resource("http://localhost:3000/songs/:id", {}, {
+      update: {method: "PUT"}
+    });
+    Song.all = Song.query();
+    return Song;
+  }
+
+  function indexCtrlFunction(Song){
+    var indexVM = this;
+    indexVM.songs = Song.all;
+  }
+
+  function showCtrlFunction(Song, $stateParams){
+    var showVM = this;
+    Song.all.$promise.then(function(){
+      Song.all.forEach(function(song){
+        if(song.id == $stateParams.id){
+          showVM.song = song;
+        }
+      });
+    });
+  }
+
 
 }());
