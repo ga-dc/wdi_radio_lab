0ff9bf27b376608d3820936bc9379ab1395be806
Christine Movius <christine.movius@gmail.com>
2016-03-31 15:38:02 -0400

    Redirects sometimes show refereshed data and sometimes don't... interesting.

--- js/songs/edit.controller.js
+++ js/songs/edit.controller.js
@@ -14,10 +14,11 @@
     this.song = songFactory.get({id: $stateParams.id});
     this.update = function(){
       this.song.$update({id: $stateParams.id});
-      // $state.go("songIndex"); This "works" but you have to refresh the page to get the changes grrr
+      $state.go("songIndex", {}, { reload: true }); //these sometimes work properly and sometimes don't work properly, don't really know why.
     }
     this.destroy = function(){
       this.song.$delete({id: $stateParams.id});
+      $state.go("songIndex", {}, { reload: true }); //these sometimes work properly and sometimes don't work properly, don't really know why.
     }
   }
 }());
