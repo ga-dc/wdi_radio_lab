4eee75141031906a8b2cc6ec11ce77a0b2d6b3b8
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-31 23:09:00 -0400

    Create new function controller and form view.

--- js/app.js
+++ js/app.js
@@ -19,6 +19,12 @@
       controller: "songIndexController",
       controllerAs: "songIndexViewModel"
     })
+    .state("songNew", {
+      url: "/new",
+      templateUrl: "js/songs/new.html",
+      controller: "newSongController",
+      controllerAs: "songNewViewModel"
+    })
     .state("songShow", {
       url: "/:id",
       templateUrl: "js/songs/show.html",
---
+++ js/songs/new.controller.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(function() {
+  angular
+  .module("songs")
+  .controller("newSongController", [
+    "songFactory"
+  ]);
+})();
