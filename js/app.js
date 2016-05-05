e34d2cf4c5f051919cf821f7a3bd371d8ac27a75
Spicy Legato <jpqreichard@gmail.com>
2016-05-05 18:58:11 -0400

    adds controller for show and index for songs and corresponding states, also factories.

--- js/app.js
+++ js/app.js
@@ -9,15 +9,76 @@
   .config([
     "$stateProvider",
     RouterFunction
-  ]);
+  ])
+  .controller("WelcomeController", WelcomeControllerFunc)
+  .controller("IndexController", IndexControllerFunc)
+  .controller("ShowController", ShowControllerFunc)
+  .factory("SongFactory", SongFactoryFunc);
+
+  SongFactoryFunc.$inject = ["$resource"];
+  function SongFactoryFunc($resource){
+    return $resource("http://localhost:3000/songs/:id.json", {}, {
+      update: {method: "PUT"}
+    });
+  }
+
 
   function RouterFunction($stateProvider){
     $stateProvider
-      .state("welcome",{
-        url: "/",
-        templateUrl: "js/welcome.template.html",
-        Controller: "welcomeController",
-        ControllerAs: "welcomeVm"
-      });
+    .state("welcome", {
+      url: "/",
+      templateUrl: "js/welcome.template.html",
+      controller: "WelcomeController",
+      controllerAs: "welcomeVm"
+    })
+    .state("indexSongs", {
+      url: "/songs",
+      templateUrl: "js/index.template.html",
+      controller: "IndexController",
+      controllerAs: "indexVm"
+    })
+    .state("showSongs", {
+      url: "/songs/:id",
+      templateUrl: "js/show.template.html",
+      controller: "ShowController",
+      controllerAs: "showVm"
+    });
   }
+
+  WelcomeControllerFunc.$inject = ["$state"];
+  function WelcomeControllerFunc($state) {
+    var welcomeVm = this;
+  }
+
+  IndexControllerFunc.$inject = ["$state", "SongFactory"];
+  function IndexControllerFunc($state, SongFactory) {
+    var indexVm = this;
+    indexVm.songs = SongFactory.query();
+    indexVm.newSong = new SongFactory();
+
+    indexVm.create = function() {
+      indexVm.newSong.$save().then(function(){
+        $state.go("indexSongs", {}, {reload: true});
+      });
+    };
+
+  }
+
+  ShowControllerFunc.$inject = ["$state", "$stateParams", "SongFactory"];
+  function ShowControllerFunc($state, $stateParams, SongFactory) {
+    var showVm = this;
+    showVm.song = SongFactory.get({id: $stateParams.id});
+
+    showVm.update = function() {
+      showVm.song.$update({id: $stateParams.id});
+    }
+
+
+    showVm.delete = function() {
+      showVm.song.$delete({id: $stateParams.id}).then(function() {
+        $state.go("indexSongs", {}, {reload: true});
+      });
+    };
+  };
+
 })();
