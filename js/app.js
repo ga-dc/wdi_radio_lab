1bda3d62f0f230cf276fd330fbbda04a03452778
Hewan Abreha <henebys@gmail.com>
2016-05-05 15:29:35 -0400

    Condense files into one and wire up songs index view

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
-"use strict";
+  "use strict";
 
-(function () {
-  angular
-  .module("WdiRadio",[
-    "ui.router",
-    "songs"
+  (function () {
+    angular
+    .module("WdiRadio",[
+      "ui.router",
+      "ngResource"
+    ])
+  .config([
+    "$stateProvider",
+    RouterFunction
   ])
-.config([
-  "$stateProvider",
-  RouterFunction
-]);
+  .controller("SongIndexController", SongIndexControllerFunc)
+  .factory("SongFactory", SongFactoryFunc)
 
-function RouterFunction($stateProvider){
-  $stateProvider
-  .state("home", {
-    url: "",
-    template: "Welcome to the World's Best Song App"
-  })
-  .state("songs", {
-    url: "/songs",
-    templateUrl: "js/songs/index.html",
-    controller: "SongsIndexController",
-    controllerAs: "SongsIndexViewModel"
-  })
-}
-}());
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/index.html",
+      controller: "SongIndexController",
+      controllerAs: "SongIndexViewModel"
+    })
+  }
+  SongFactoryFunc.$inject = ["$resource"]
+  function SongFactoryFunc($resource) {
+    return $resource("http://localhost:3000/songs/:id", {}, {
+      update: {method: "PUT"}
+    });
+  }
+  SongIndexControllerFunc.$inject = ["SongFactory"]
+  function SongIndexControllerFunc(SongFactory){
+    this.songs = SongFactory.query();
+  }
+  }());
