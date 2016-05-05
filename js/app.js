5e223b9c7162a6c9461b2f3dfb21b9329f8afb73
Leland Jordon <lelandjordon@mac.com>
2016-05-05 11:46:14 -0400

    got Hello Dave to work from the template.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,30 @@
 
 (function(){
   angular
-    .module("app", ["ui.router"])
+    .module("wdi_radio", [
+      "ui.router"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunc
+    ])
+    .controller("SongsIndexCtrl", SongsIndexCtrlFunc)
 
+    function RouterFunc($stateProvider) {
+
+      $stateProvider
+        .state("allSongs", {
+          url: "/songs",
+          templateUrl: "/js/index.template.html",
+          controller: "SongsIndexCtrl",
+          controllerAs: "indexVm"
+        })
+    }
+
+
+    function SongsIndexCtrlFunc() {
+      var indexVm = this;
+      indexVm.hello = "Hello Dave"
+    }
 
 })()
