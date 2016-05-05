6456ebf4fe8ad64d73efd26a6629359b879b10d2
Wes Reed <wesley.a.reed@gmail.com>
2016-05-05 10:28:23 -0400

    Wire up app with router function

---
+++ js/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+(function (){
+  angular
+    .module("wdiRadio", [
+      "ui.router",
+      "songs"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ]);
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+        .state("home", {
+          url: "",
+          template: "Welcome to the World's Best Song App"
+        })
+        .state("songs", {
+          url: "/songs",
+          templateUrl: "js/songs/index.html",
+          controller: "SongsIndexController",
+          controllerAs: "SongsIndexViewModel"
+        })
+    }
+}());
