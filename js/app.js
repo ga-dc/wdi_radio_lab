ed2da600785232fcc266240212be385bdefa9fe0
Hewan Abreha <henebys@gmail.com>
2016-05-08 19:38:16 -0400

    modified js files and index, add home state

--- js/app.js
+++ js/app.js
@@ -1,50 +1,70 @@
-  "use strict";
+"use strict";
 
-  (function () {
-    angular
-    .module("WdiRadio",[
-      "ui.router",
-      "ngResource"
-    ])
+(function (){
+  angular
+  .module("wdiRadio", [
+    "ui.router",
+    "ngResource"
+  ])
   .config([
     "$stateProvider",
     RouterFunction
   ])
   .factory("SongFactory", SongFactoryFunc)
   .controller("SongIndexController", SongIndexControllerFunc)
-  .controller("SongShowController", SongShowControllerFunc)
+  .controller("SongShowController",  SongShowControllerFunc)
+
+
 
   function RouterFunction($stateProvider){
     $stateProvider
-    .state("songIndex", {
+    .state("home", {
+      url: "",
+      template: "Welcome to the Best Song App in the World"
+    })
+    .state("songsIndex", {
       url: "/songs",
       templateUrl: "js/index.html",
       controller: "SongIndexController",
       controllerAs: "SongIndexViewModel"
     })
-
     .state("songShow", {
-      url: "/songs/id",
+      url: "/songs/:id",
       templateUrl: "js/show.html",
       controller: "SongShowController",
-      controllerAs: "SongShowControllerViewModel"
-  })
-}
+      controllerAs: "SongShowViewModel"
+    })
+  }
 
   SongFactoryFunc.$inject = ["$resource"]
   function SongFactoryFunc($resource) {
     return $resource("http://localhost:3000/songs/:id", {}, {
-      update: {method: "PUT"}
+      update: {method:  "PUT"}
     });
   }
 
   SongIndexControllerFunc.$inject = ["SongFactory"]
-  function SongIndexControllerFunc(SongFactory){
+  function SongIndexControllerFunc (SongFactory){
     this.songs = SongFactory.query();
+    this.newSong = new SongFactory();
+
+    this.create = function($state){
+      this.newSong.$save().then(function(response){
+        this.songs.push(response)
+        this.newSong = new SongFactory();
+      })
+    };
   }
 
-  SongShowControllerFunc.$inject = ["SongFactory"]
-  function SongShowControllerFunc (SongFactory) {
-    console.log("Yey it is working");
-  }
-  }());
+  SongShowControllerFunc.$inject = ["SongFactory", "$stateParams"]
+  function SongShowControllerFunc (SongFactory, $stateParams) {
+    this.song = SongFactory.get({id: $stateParams.id});
+
+    this.update = function(){
+      this.song.$update({id: $stateParams.id});
+    };
+    this.delete = function(){
+      this.song.$delete({id: $stateParams.id})
+    }
+  };
+}());
