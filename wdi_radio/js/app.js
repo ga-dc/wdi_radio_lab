7b0d861019a8392a6213281c70a34c06bfac879e
Matthew Scott <matthewruelscott@gmail.com>
2016-07-14 10:14:54 -0400

    Initial commit

---
+++ wdi_radio/js/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+(function(){
+  angular
+  .module("wdi_radio", [
+    "ui.router",
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/songs/index.html",
+      controller: "SongIndexController",
+      controllerAs: "SongIndexViewModel"
+    });
+  }
+})
