d12aa7641f942f0df2f4d621781be99cfac49489
Anh Truong <anh.truong.07@cnu.edu>
2016-07-14 18:29:43 -0400

    added landing

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,10 @@
 
     function RouterFunction($stateProvider){
       $stateProvider
+      .state("landing", {
+        url: "/",
+        templateUrl: "js/mixes/landing.html"
+      })
       .state("mixIndex", {
         url: "/mixes",
         templateUrl: "js/mixes/index.html",
