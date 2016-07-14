f046fd04e891310b969967f449b564c6b8adc6a5
Anh Truong <anh.truong.07@cnu.edu>
2016-07-14 14:54:26 -0400

    data index view works

--- public/js/app.js
+++ public/js/app.js
@@ -17,13 +17,13 @@
       .state("mixIndex", {
         url: "/mixes",
         templateUrl: "js/mixes/index.html",
-        controller: "MixesIndexController",
-        controllerAs: "MixesIndexViewModel"
+        controller: "MixIndexController",
+        controllerAs: "MixIndexViewModel"
       })
       .state("mixShow", {
         url: "/mixes/:id",
         templateUrl: "js/mixes/show.html",
-        controller: "MixesShowController",
+        controller: "MixShowController",
         controllerAs: "MixShowViewModel"
       });
     }
--- public/js/mixes/mixes.js
+++ public/js/mixes/mixes.js
@@ -2,5 +2,7 @@
 
 (function(){
   angular
-  .module("mixes", []);
+  .module("mixes", [
+    "firebase"
+  ]);
 }());
