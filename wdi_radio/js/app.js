5b531b5e0326428f47cc3457f345760ada0086cf
Matthew Scott <matthewruelscott@gmail.com>
2016-07-14 11:42:53 -0400

    Adds create/delete functionality

--- wdi_radio/js/app.js
+++ wdi_radio/js/app.js
@@ -18,12 +18,6 @@
       templateUrl: "js/songs/index.html",
       controller: "SongIndexController",
       controllerAs: "SongIndexViewModel"
-    })
-    .state("songShow", {
-      url: "/songs/:id",
-      templateUrl: "js/songs/show.html",
-      controller: "SongShowController",
-      controllerAs: "SongShowViewModel"
     });
   }
 }());
