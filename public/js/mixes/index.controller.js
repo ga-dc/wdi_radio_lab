c1139853d19015155c34e8eab376451840eb50d7
Anh Truong <anh.truong.07@cnu.edu>
2016-07-14 17:52:52 -0400

    css kinda works

--- public/js/mixes/index.controller.js
+++ public/js/mixes/index.controller.js
@@ -3,12 +3,19 @@
   (function(){
     angular
     .module("mixes")
+    .filter("unsafe", ["$sce", function($sce){
+      return $sce.trustasHtml
+    }])
     .controller("MixIndexController", [
       "$firebaseArray",
       MixIndexControllerFunction
     ]);
 
     function MixIndexControllerFunction($firebaseArray){
+      // this.break = function(string){
+      //   return string.replace(/NEWLINE/g, "<br>")
+      //
+      // }
       var vm = this;
       var ref = firebase.database().ref().child("mixes");
       vm.mixes = $firebaseArray(ref);
