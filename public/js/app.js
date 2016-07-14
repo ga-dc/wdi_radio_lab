75ee1b633f90866fb873d002103bd15aea5fa3dd
Adina Pollack <adinap16@gmail.com>
2016-07-14 12:33:19 -0400

    CRUD

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+(function(){
+  angular
+    .module("radio", [
+      "ui.router",
+      "songs"
+    ])
+
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ]);
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+      .state("welcome", {
+        url: "/",
+        templateUrl: "/public/js/wdi_radio/root.html",
+      })
+      .state("songIndex", {
+        url: "/songs",
+        templateUrl: "/public/js/wdi_radio/index.html",
+        controller: "SongIndexController",
+        controllerAs: "SongIndexViewModel"
+      })
+      .state("songShow", {
+        url: "/songs/:id",
+        templateUrl: "/public/js/wdi_radio/show.html",
+        controller: "SongShowController",
+        controllerAs: "SongShowViewModel"
+      })
+    }
+})();
