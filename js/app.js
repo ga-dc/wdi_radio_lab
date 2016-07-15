2d6812b810be1740c8535ffa9d9fa7b230f17a18
Mara Sanders <sandersmara@gmail.com>
2016-07-14 21:56:18 -0400

    Initial commit - showing pictures and create are functional

---
+++ js/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+(function(){
+  angular
+   .module("radio",[
+    "ui.router",
+    "songs"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+     .state("welcome", {
+     url: "",
+     templateUrl: "/js/songs/welcomePage.html"
+  })
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "/js/songs/index.html",
+      controller: "SongIndexController",
+      controllerAs: "SongIndexViewModel"
+    })
+    .state("songShow", {
+      url: "/songs/:id",
+      templateUrl: "/js/songs/show.html",
+      controller: "SongShowController",
+      controllerAs: "SongShowViewModel"
+    })
+  }
+})();
