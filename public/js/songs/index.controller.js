0d724edd345b1b5ec80ffc45f005f85efb2508f6
Tyler Rosenberg <tyryanr@gmail.com>
2016-08-18 20:25:05 -0400

    got index view for all songs

--- public/js/songs/index.controller.js
+++ public/js/songs/index.controller.js
@@ -4,10 +4,13 @@
   angular
   .module("songs")
   .controller( "SongsIndexController", [
+    "$firebaseArray",
     SongsIndexControllerCallback
   ])
 
-  function SongsIndexControllerCallback() {
-    console.log("works!");
+  function SongsIndexControllerCallback($firebaseArray) {
+    var vm = this;
+    var ref = firebase.database().ref().child("songs");
+    vm.songs = $firebaseArray(ref)
   }
 }());
