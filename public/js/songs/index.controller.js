03f72fbf4d212bfbb14c087b2e21e839517fb590
pamelaolney <pjolney12@gmail.com>
2016-08-18 23:21:00 -0400

    attempting to get songs to show up again

---
+++ public/js/songs/index.controller.js
@@ -0,0 +1,17 @@
+"use strict";
+
+(function(){
+  angular
+  .module("songs")
+  .controller("SongIndexController", [
+    "$firebaseArray",
+    SongIndexControllerFunction
+  ]);
+
+
+  function SongIndexControllerFunction($firebaseArray){
+    var vm = this;
+    var ref = firebase.database().ref().child("songs");
+    vm.songs = $firebaseArray(ref);
+  }
+}());
