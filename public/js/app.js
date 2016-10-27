8ccc3b52cd5ec348bd8e86d84df9ebbd755eedcd
Michael Lai <mike24567@gmail.com>
2016-10-27 10:57:51 -0400

    added index page

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,28 @@
 angular
-  .module("radio", [])
-  
+  .module("radio", [
+    "ui.router",
+    "firebase"
+  ])
+  .config([
+    "$stateProvider",
+    Router
+  ])
+  .controller("SongIndexCtrl", [
+    "$firebaseArray",
+    songController
+  ])
+
+  function songController($firebaseArray){
+    let ref = firebase.database().ref().child("songs")
+    this.songs = $firebaseArray(ref)
+  }
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("songIndex",{
+      url: "/songs",
+      templateUrl: "js/ng-views/index.html",
+      controller: "SongIndexCtrl",
+      controllerAs: "vm"
+    })
+  }
