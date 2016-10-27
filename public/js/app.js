27d63d513602f5980adb5d498a908b3b6ceb50eb
Alexander <ajspottswood@gmail.com>
2016-10-27 14:45:33 -0400

    data.json created

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,25 @@ angular
     "stateProvider",
     RouterFunction
   ])
-  .controller("RadioIndexController", [
-    RadioIndexControllerFunction
+  .controller("SongIndexController", [
+    SongIndexControllerFunction
   ])
 
-  function RadioIndexControllerFunction(){
-    
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/ng-views/index.html",
+      controller: "SongIndexController",
+      controllerAs: "vm"
+    })
+    .state("songShow", {
+      url: "songs/:id",
+      templateUrl: "js/ng-views/show.html"
+    })
+  }
+
+
+  function SongIndexControllerFunction(){
+
   }
