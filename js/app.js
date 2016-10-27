2385c525520d88294fc0c74c0b6e7127d94bca44
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-27 14:55:06 -0400

    update

--- js/app.js
+++ js/app.js
@@ -4,28 +4,64 @@ angular.module("wdiradio", [
 ])
 .config([
   "$stateProvider",
-  Router
+  Router,
 ])
 .controller("SongIndexController", [
   "$firebaseArray",
+  "$firebaseObject",
   SongIndexControllerFunction
 ])
+// .controller("SongShowController",[
+//     "$stateParams",
+//     "$firebaseObject",
+//     SongShowControllerFunction
+// ])
 
-function SongIndexControllerFunction($firebaseArray){
+
+function SongIndexControllerFunction($firebaseArray, $firebaseObject){
   let ref = firebase.database().ref().child("songs")
   this.songs = $firebaseArray(ref)
+  this.newTodo = {}
 
   this.create = function(){
-    this.songs.$add(this.newSong).then(()=>this.newSong)
+    this.songs.$add(this.newSong).then(()=>this.newSong = {})
   }
+
+  this.update = function(song){
+    console.log("update!")
+    this.songs.$save(song)
+  }
+
 }
 
-function Router($stateProvider){
-  $stateProvider
-  .state("songIndex", {
-    url: "/songs",
-    templateUrl: "js/ng-views/index.html",
-    controller: "SongIndexController",
-    controllerAs: "vm"
-  })
+function NewSongShow(){
+  if (menuShow = true){
+    menuShow = false
+  }
+  else{
+  menuShow = true
 }
+}
+
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/ng-views/index.html",
+      controller: "SongIndexController",
+      controllerAs: "vm"
+    })
+    .state("songNew", {
+      url: "/songs/new",
+      templateUrl: "js/ng-views/new.html",
+      controller: "SongIndexController",
+      controllerAs: "vm"
+    })
+    // .state("songShow", {
+    //   url: "/songs/:id",
+    //   templateUrl: "js/ng-views/show.html",
+    //   controller: "SongShowController",
+    //   ControllerAs: "vm"
+    // })
+  }
