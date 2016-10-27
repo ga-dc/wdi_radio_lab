9e166d496b94fc201068927251c381d053e3c876
William Keating <keating49@gmail.com>
2016-10-27 12:05:58 -0400

    crud and show for the most part

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ angular
 ])
 
 .config([
-  "$stateProvider"
+  "$stateProvider",
   RouterFunction
 ])
 
@@ -13,3 +13,44 @@ angular
   "$firebaseArray",
   wdiRadioIndexControllerFunction
 ])
+.controller("wdiRadioShowController", [
+    "$stateParams",
+    "$firebaseObject",
+    wdiRadioShowControllerFunction
+])
+
+
+function RouterFunction($stateProvider) {
+  $stateProvider
+    .state("wdiRadioIndex", {
+      url: "/wdiRadio",
+      templateUrl: "js/ng-views/index.html",
+      controller: "wdiRadioIndexController",
+      controllerAs: "vm"
+    })
+    .state("wdiRadioShow", {
+        url: "/wdiRadio/:id",
+        templateUrl: "js/ng-views/show.html",
+        controller: "wdiRadioShowController",
+        controllerAs: "vm"
+    })
+}
+
+
+function wdiRadioIndexControllerFunction($firebaseArray){
+    let ref = firebase.database().ref();
+    this.wdiRadio = $firebaseArray(ref);
+    this.create = function(){
+      this.wdiRadio.$add(this.newwdiRadio).then( () => this.newwdiRadio = {} )
+    }
+    this.delete = function(wdiRadio){
+      this.wdiRadio.$remove(wdiRadio)
+    }
+  }
+  function wdiRadioShowControllerFunction($stateParams, $firebaseObject){
+      let ref = firebase.database().ref("/" + $stateParams.id);
+      $firebaseObject(ref).$loaded().then(wdiRadio => this.wdiRadio = wdiRadio)
+      this.update = function(){
+        this.wdiRadio.$save();
+      }
+    }
