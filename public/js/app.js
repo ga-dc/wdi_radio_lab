43b3530be5e3457ae1e36db60d472009822cf73a
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-27 19:56:42 -0400

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,67 +1,52 @@
 angular
-  .module("wdiRadio", [
-    "ui.router",
-    "firebase",
-    "songs"
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ])
-  .controller("WelcomeController", [
-    "$state",
-    WelcomeControllerFunction
-  ])
-  .controller("SongIndexController", [
-    "$firebaseArray",
-    SongIndexControllerFunction
-  ])
-  .controller("SongShowController", [
-    "$stateParams",
-    "$firebaseObject",
-    SongShowControllerFunction
-  ])
+.module("wdiRadio", [
+  "ui.router",
+  "firebase",
+])
+.config([
+  "$stateProvider",
+  RouterFunction
+])
+.controller("SongsIndexController", [
+  "$firebaseArray",
+  SongsIndexControllerFunction
+])
+.controller("SongsShowController", [
+  "$stateParams",
+  "$firebaseArray"
+])
 
-  function RouterFunction($stateProvider) {
-    $stateProvider
-    .state("welcome", {
-      url: "/",
-      templateUrl: "js/songs/welcome.html"
-    })
-    .state("songIndex", {
-      url: "/songs",
-      templateUrl: "js/songs/index.html",
-      controller: "SongIndexController",
-      controllerAs: "vm"
-    })
-    .state("songShow", {
-      url: "/songs/:id",
-      templateUrl: "js/songs/show.html",
-      controller: "SongShowController",
-      controllerAs: "vm"
-    });
-  }
+function RouterFunction($stateProvider) {
+  $stateProvider
+  .state("songsIndex", {
+    url: "/songs",
+    templateUrl: "js/ng-views/index.html",
+    controller: "SongsIndexController",
+    controllerAs: "vm"
+  })
+  .state("songsShow", {
+    url: "/songs/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "SongsIndexController",
+    controllerAs: "vm"
+  })
+}
+function SongsIndexControllerFunction($firebaseArray) {
+  let ref = firebase.database().ref().child("songs");
+  this.songs = $firebaseArray(ref);
 
-  function WelcomeControllerFunction() {
-
-  }
-
-  function SongIndexControllerFunction($firebaseArray) {
-    let ref = firebase.database().ref().child("wdiRadio");
-    this.songs = $firebaseArray(ref);
-
-  }
-  function SongShowControllerFunction($stateParams, $firebaseObject){
-    let ref = firebase.database().ref().child("wdiRadio/" + $stateParams.id);
-    $firebaseObject(ref).$loaded().then(song => this.song = song)
-  }
-
-  // #create
-  this.create = function(){
+  this.create = function() {
     this.songs.$add(this.newSong).then( () => this.newSong = {} )
-    console.log("clicked")
+    console.log("hello"); //not printing
   }
-  // #destroy
-  this.delete = function(song){
-    this.songs.$remove(song)
+}
+function SongsShowControllerFunction($stateParams, $firebaseObject){
+  let ref = firebase.database().ref().child("songs/" + $stateParams.id);
+  $firebaseObject(ref).$loaded().then(song => {
+    this.song = song
+  })
+  this.update = function(){
+    this.song.$save();
+
   }
+}
