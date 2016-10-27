32341ec066926a15488dc87b96eadf0f8ec7a3fd
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-27 10:24:19 -0400

    data and index

---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+angular
+  .module("radioApp", [
+    "ui.router",
+    "firebase"
+  ])
+  .config([
+    "stateProvider",
+    RouterFunction
+  ])
+  .controller("RadioIndexController",[
+    "$firebaseArray"
+    RadioIndexControllerFunction
+  ])
+  .controller("RadioShowController", [
+    "$stateParams",
+    "$firebaseObject",
+    RadioShowControllerFunction
+  ])
+
+  function RadioShowControllerFunction($stateParams, $firebaseObject){
+      let ref = firebase.database().ref().child("songs/" + $stateParams.id);
+
+$firebaseObject(ref).$loaded().then(grumble => this.song = song)
+}
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("songIndex", {
+    url: "/songs",
+    templateUrl: "js/ng-views/index.html",
+    controller: "RadioIndexController",
+    controllerAs:"vm"
+  })
+  .state("songShow", {
+    url: "/songs/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "RadioShowController",
+    controllerAs:"vm"
+  })
+}
+
+function RadioIndexControllerFunction($firebaseArray){
+    let ref = firebase.database().ref().child("songs");
+    this.songs = $firebaseArray(ref);
+
+    this.create = function () {
+      this.songs.$add(this.newSong).then( () => this.newSong = {} )
+    }
+
+    this.delete = function(song){
+      this.songs.$remove(song)
+    }
+  }
