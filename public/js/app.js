f12aa09ff79e02f018a271d672134d289fa3333d
Michael Lai <mike24567@gmail.com>
2016-10-27 14:35:52 -0400

    added full CRUD functionality

--- public/js/app.js
+++ public/js/app.js
@@ -9,16 +9,37 @@ angular
   ])
   .controller("SongIndexCtrl", [
     "$firebaseArray",
+    "$firebaseObject",
+    "$state",
     songController
   ])
 
-  function songController($firebaseArray){
+  function songController($firebaseArray,$firebaseObject,$state){
     let ref = firebase.database().ref().child("songs")
+    // $firebaseObject(ref).$loaded().then(song => this.song = song)
     this.songs = $firebaseArray(ref)
+    this.newSong = {}
+
+    this.add = function(){
+      // this.addSong = false
+      this.songs.$add(this.newSong).then( () => this.newSong = {})
+    }
+    this.update = function(song){
+      // this.editSong = false
+      this.songs.$save(song)
+    }
+    this.remove = function(song){
+      this.songs.$remove(song)
+    }
+
   }
 
   function Router($stateProvider){
     $stateProvider
+    .state("home",{
+      url: "",
+      templateUrl: "js/ng-views/home.html"
+    })
     .state("songIndex",{
       url: "/songs",
       templateUrl: "js/ng-views/index.html",
