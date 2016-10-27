19a9ccc886511c331e044220086a7812409db75f
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-27 12:28:51 -0400

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
-angular
+"use strict"
+(function() {
+  angular
   .module("wdiRadio", [
     "ui.router",
     "firebase",
@@ -8,26 +10,44 @@ angular
     "$stateProvider",
     RouterFunction
   ])
-  .controller("SongsIndexController", [
+  .controller("SongIndexController", [
     "$firebaseArray",
-    SongsIndexControllerFunction
+    SongIndexControllerFunction
+  ])
+  .controller("SongShowController", [
+    "$stateParams",
+    "$firebaseObject",
+    SongShowControllerFunction
   ])
 
   function RouterFunction($stateProvider) {
     $stateProvider
-      .state("Welcome", {
-        url: "",
-        templateUrl: "js/welcome.html"
-      })
-      .state("songs", {
-        url: "/songs",
-        templateUrl: "js/songs/index.html",
-        controller: "SongsIndexController",
-        controllerAs: "vm"
-      })
+    .state("Welcome", {
+      url: "/",
+      templateUrl: "js/welcome.html"
+    })
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/songs/index.html",
+      controller: "SongIndexController",
+      controllerAs: "vm"
+    })
+    .state("songShow", {
+      url: "/songs/:id",
+      templateUrl: "js/songs/show.html",
+      controller: "SongShowController",
+      controllerAs: "vm"
+    });
   }
 
-  function SongsIndexControllerFunction($firebaseArray) {
+  function SongIndexControllerFunction($firebaseArray) {
     let ref = firebase.database().ref().child("songs");
     this.songs = $firebaseArray(ref);
   }
+
+  function SongShowControllerFunction($stateParams, $firebaseObject) {
+    let ref = firebase.database().ref().child("songs/" + $stateParams.id);
+  }
+
+
+}());
