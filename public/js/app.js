dcbdfffa4dd5e565a3762cb3bba279859d155494
Eva Lai <evalai.dc@gmail.com>
2017-03-09 12:42:31 -0500

    added update delete and create functionality

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,11 @@ angular
     "$firebaseArray",
     SongIndexControllerFunction
   ])
+  .controller("SongShowController", [
+    "$stateParams",
+    "$firebaseObject",
+    SongShowControllerFunction
+  ])
 
 
 function RouterFunction($stateProvider){
@@ -22,10 +27,32 @@ function RouterFunction($stateProvider){
     controller: "SongIndexController",
     controllerAs: "vm"
   })
+  .state("songShow", {
+    url: "/songs/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "SongShowController",
+    controllerAs: "vm"
+  })
 }
 
 
 function SongIndexControllerFunction($firebaseArray) {
   let ref= firebase.database().ref().child("songs")
   this.songs = $firebaseArray(ref);
+  this.create = function(){
+    this.songs.$add(this.newSong).then( () => this.newSong = {} )
+  }
+
+  this.update = function(song) {
+    this.songs.$save(song)
+  }
+
+  this.delete = function(song) {
+    this.songs.$remove(song)
+  }
+}
+
+function SongShowControllerFunction($stateParams, $firebaseObject) {
+  let ref= firebase.database().ref().child("songs/" + $stateParams.id);
+  $firebaseObject(ref).$loaded().then( song => this.song = song)
 }
