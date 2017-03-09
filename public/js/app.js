f5919fe8ff878f51a53d9730d1bc272ed3356017
Erica Norby <ericanorby@gmail.com>
2017-03-09 11:22:12 -0500

    firebase and router set up

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,27 @@
 angular
   .module("wdiRadio", [
     "firebase",
-    "ui-router"
+    "ui.router"
   ])
   .config([
     "$stateProvider",
     RouterFunction
   ])
+  .controller("SongsIndexController", [
+    "$firebaseArray",
+    SongsIndexControllerFunction
+  ])
 
 function RouterFunction($stateProvider){
+  $stateProvider
+  .state("songsIndex", {
+    url: "/songs",
+    templateUrl: "../ng-views/index.html",
+    controller: "SongsIndexController",
+    controllerAs: "vm"
+  })
+}
 
+function SongsIndexControllerFunction($firebaseArray){
+  let ref = firebase.database().ref().child("songs");
 }
