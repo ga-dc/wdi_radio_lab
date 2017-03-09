ab7bd59821a927382fa32ed30778d78592a204df
Chris Wightman <wightman_christopher@bah.com>
2017-03-09 15:11:15 -0500

    app running on firebase. Both index.html working. Show is not complete. cannot play songs.

---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+angular
+  .module("radio", [
+    "ui.router",
+    "firebase",
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("SongIndexController", [
+    "$firebaseArray",
+    SongIndexControllerFunction
+  ])
+  .controller("SongShowController", [
+      "$stateParams",
+      "firebaseObject",
+      SongShowControllerFunction
+  ])
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("songIndex", {
+    url: "/songs",
+    templateUrl: "js/ng-views/index.html",
+    controller: "SongIndexController",
+    controllerAs: "vm"
+  })
+  .state("songShow", {
+    url: "/songs/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "SongShowController",
+    controllerAs: "vm"
+  })
+}
+
+function SongIndexControllerFunction($firebaseArray){
+  let ref = firebase.database().ref().child("songs");
+  this.songs = $firebaseArray(ref);
+
+  this.delete = function(song){
+    this.songs.$remove(song)
+  }
+}
+  function SongShowControllerFunction($stateParams, $firebaseObject){
+      // This time, ref contains a reference to a specific grumble.
+      let ref = firebase.database().ref().child("songs/" + $stateParams.id);
+
+      // Then we retrieve a $firebaseObject based on ref. Once that asynchronous action is done, we save the resulting grumble to `vm.grumble`.
+      $firebaseObject(ref).$loaded().then( song => this.song = song )
+    }
