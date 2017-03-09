71d9371949d9c83e750abdb63ae867659ecef218
Samuel Clements <sdkclements@gmail.com>
2017-03-09 14:08:48 -0500

    had my ngapp in the wrong place for like 2 hours

--- public/js/app.js
+++ public/js/app.js
@@ -1,40 +1,45 @@
 "use strict";
 
-(function() {
 
-    angular.module("radiosc", [
-            "ui.router",
-            "firebase"
-        ])
-        .config([
-            "$stateProvider",
-            RouterFunction
-        ])
 
-    .controller("SongIndexController", [
-        "$firebaseArray",
-        SongIndexControllerFunction
+angular
+    .module("radiosc", [
+        "ui.router",
+        "firebase"
+    ])
+    .config([
+        "$stateProvider",
+        RouterFunction
     ])
 
-
-    function RouterFunction($stateProvider) {
-        $stateProvider
-            .state("SongIndex", {
-                url: '/',
-                templateUrl: "js/ng-views/index.html",
-                controller: "SongIndexController",
-                controllerAs: "vm"
-            })
-    }
-
-    function SongIndexControllerFunction($firebaseArray) {
-        let ref = firebase.database().ref().child("songs");
-        this.songs = $firebaseArray
-    }
+.controller("SongIndexController", [
+    "$firebaseArray",
+    SongIndexControllerFunction
+])
 
 
+function RouterFunction($stateProvider) {
+    $stateProvider
+        .state("home", {
+            url: "/",
+            templateUrl: "js/ng-views/home.html",
+            controller: "HomeController",
+            controllerAs: "vm"
+        })
+        .state("songIndex", {
+            url: "/songs",
+            templateUrl: "js/ng-views/index.html",
+            controller: "SongIndexController",
+            controllerAs: "vm"
+        })
+}
 
+function HomeControllerFunction() {
 
+}
 
-
-})();
+function SongIndexControllerFunction($firebaseArray) {
+    console.log('this is happening')
+    let ref = firebase.database().ref().child("songs");
+    this.songs = $firebaseArray(ref)
+}
