70e4a1f12a42e864d26c2b3e0c97096f41105d04
Fiona <h.natapra@gmail.com>
2017-03-09 14:00:16 -0500

    firebase setup,initialize router, controller, index views, attempted show controlla and view

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,55 @@
 angular
   .module("wdiradio", [
+    "firebase",
     "ui.router",
-    "ngResource"
   ])
   .config([
     "$stateProvider",
     RouterFunction
   ])
-  
+  .controller("WdiradioController", [
+    "$firebaseArray",
+    WdiradioControllerFunction
+  ])
+  .controller("WdiradioIndexController", [
+    "$firebaseArray",
+    WdiradioIndexControllerFunction
+  ])
+  // .controller("WdiradioShowController", [
+  //   "$firebaseArray",
+  //   "$stateParams",
+  //   WdiradioShowControllerFunction
+  // ])
+
+
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("wdiradioIndex", {
+      url: "/wdiradio/songs",
+      templateUrl: "js/ng-views/index.html",
+      controller: "WdiradioIndexController",
+      controllerAs: "vm"
+    })
+    // .state("wdiradioShow", {
+    //   url: "/wdiradio/:id",
+    //   templateUrl: "js/ng-views/show.html",
+    //   controller: "WdiradioShowController",
+    //   controllerAs: "vm"
+    // })
+  }
+
+function WdiradioControllerFunction($firebaseArray) {
+  let ref = firebase.database().ref().child("songs");
+  this.songs = $firebaseArray(ref);
+
+}
+
+function WdiradioIndexControllerFunction($firebaseArray) {
+  let ref = firebase.database().ref().child("songs");
+  this.songs = $firebaseArray(ref);
+}
+
+// function WdiradioShowControllerFunction($firebaseArray, $stateParams) {
+//   let ref = firebase.database().ref().child("songs").get({id: $stateParams.id});
+//   this.song = $firebaseArray(ref)
+// }
