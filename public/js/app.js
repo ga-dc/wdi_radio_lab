e5a6ffe10ccd636333d6a87ac52334616cee8ced
Andy Flickinger <atflick@gmail.com>
2017-03-09 14:02:01 -0500

    full crud

--- public/js/app.js
+++ public/js/app.js
@@ -40,4 +40,14 @@ function WelcomeControllerFunction() {
 function SongsControllerFunction($firebaseArray) {
   let ref = firebase.database().ref().child("songs");
   this.songs = $firebaseArray(ref);
+  this.newSong = {};
+  this.create = function() {
+    this.songs.$add(this.newSong).then(() => this.newSong = {})
+  }
+  this.update = function(song) {
+    this.songs.$save(song)
+  }
+  this.delete = function(song) {
+    this.songs.$remove(song)
+  }
 }
