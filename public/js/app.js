ef7f163b8bc72e75f862608ba36f92cdb59a840a
Nat Carter <n.goldman.carter@gmail.com>
2017-03-09 10:26:52 -0500

    added welcome route, controller, and view

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@ angular
     "$stateProvider",
     RouterFunction
   ])
+  .controller("WelcomeController", [
+    WelcomeControllerFunction
+  ])
   .controller("SongIndexController", [
     "$firebaseArray",
     SongIndexControllerFunction
@@ -14,12 +17,21 @@ angular
 
 function RouterFunction($stateProvider) {
   $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "js/ng-views/welcome.html",
+      controller: "WelcomeController",
+      controllerAs: "vm"
+    })
     .state("songIndex", {
       url: "/songs",
       templateUrl: "js/ng-views/index.html",
       controller: "SongIndexController",
       controllerAs: "vm"
     })
+}
+
+function WelcomeController() {
 }
 
 function SongIndexControllerFunction($firebaseArray) {
