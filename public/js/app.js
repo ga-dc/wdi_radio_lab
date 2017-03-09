a8da1a0db8fb1002db5f53bfc047751fad6d6ddd
Erica Norby <ericanorby@gmail.com>
2017-03-09 13:57:24 -0500

    added show view

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,11 @@ angular
     "$firebaseArray",
     SongsIndexControllerFunction
   ])
+  .controller("SongsShowController", [
+    "$firebaseObject",
+    "$stateParams",
+    SongsShowControllerFunction
+  ])
 
 function RouterFunction($stateProvider){
   $stateProvider
@@ -20,9 +25,20 @@ function RouterFunction($stateProvider){
     controller: "SongsIndexController",
     controllerAs: "vm"
   })
+  .state("songsShow", {
+    url: "/songs/:id",
+    templateUrl: "../ng-views/show.html",
+    controller: "SongsShowController",
+    controllerAs: "vm"
+  })
 }
 
 function SongsIndexControllerFunction($firebaseArray){
   let ref = firebase.database().ref().child("songs");
   this.songs = $firebaseArray(ref);
+}
+
+function SongsShowControllerFunction($firebaseObject, $stateParams){
+  let ref = firebase.database().ref().child("songs/" + $stateParams.id);
+  $firebaseObject(ref).$loaded().then(song => this.song = song);
 }
