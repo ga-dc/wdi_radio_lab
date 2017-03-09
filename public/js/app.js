0d5e359422cb1fb3e72a608b5a0d19456dd1d2d5
Habtamu Abere <habbuya@gmail.com>
2017-03-09 12:22:23 -0700

    views set

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,49 @@
 angular
-.module("radioApp", [])
-.controller("RadioController",
-[RadioControllerFunction]
-)
+.module("radioApp", [
+  "firebase",
+  "ui.router"
+   ])
+.config([
+  "$stateProvider",
+  RouterFunction
+])
+.controller("RadioController",[
+  "firebaseArray",
+  RadioControllerFunction
+])
+
+.controller("RadioIndexController",[
+  "$firebaseArray",
+  RadioIndexControllerFunction
+]);
 
 
 
+function RouterFunction($stateProvider) {
+  $stateProvider
+  .state ("songsIndex", {
+  url: "/songs",
+  templateUrl: "js/ng-views/index.html",
+  controller: "RadioIndexController",
+  controllerAs: "vm"
+});
+}
 
 
-function RadioControllerFunction(){
-  this.songs = Songs.all
+function RadioControllerFunction($firebaseArray){
+
+  let ref = firebase.database().ref().child("songs")
+  this.songs = $firebaseArray(ref)
+  console.log(this.songs)
+  this.newSong = {}
+
+}
+
+function RadioIndexControllerFunction($firebaseArray){
+
+  let ref = firebase.database().ref().child("songs")
+  this.songs = $firebaseArray(ref)
+  console.log(this.songs)
+  this.newSong = {}
+
 }
