aaf9ebae6d36fd28e9af4a3fdfbf3943c3ae50e7
KEVIN KELLER <keller_kevin@bah.com>
2017-03-09 15:31:03 -0500

    almost there

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,33 @@
-"use strict"
+// "use strict"
+
 
 angular
-  .module("wdiRadio", [
-    "ui.router",
-    "ngResource"
-  ])
+  .module("wdiRadio", ["ui.router", "firebase"])
+  .config(["$stateProvider", RouterFunction])
+  .controller("RadioIndexController", ["$firebaseArray", RadioIndexControllerFunction])
+  .controller("RadioShowController", ["$stateParams","$firebaseObject", RadioShowControllerFunction])
+
+  function RouterFunction($stateProvider) {
+    $stateProvider
+      .state("radioIndex", {
+          url: "/radio",
+          templateUrl: "js/ng-views/index.html",
+          controller: "RadioIndexController",
+          controllerAs: "vm"
+      })
+      .state("radioShow", {
+          url: "/radio/:id",
+          templateUrl: "js/ng-views/show.html",
+          controller: "RadioShowController",
+          controllerAs: "vm"
+      })
+  }
+
+function RadioIndexControllerFunction($firebaseArray) {
+  let ref = firebase.database().ref().child("radio")
+  this.radio = $firebaseArray(ref)
+}
+
+function RadioShowControllerFunction($stateParams, $firebaseObject) {
+  let ref = firebase.database().ref().child("radio/" + $stateParams.id)
+}
