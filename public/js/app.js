4c7188a203d7f24a28781d2b58d74de37a12b786
Dan Lin <dlin2102@gmail.com>
2017-03-09 14:39:10 -0500

    fixed error

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,32 @@
 angular
   .module("wdiradio",[
     "ui.router",
-    firebase
+    "firebase"
 
   ])
-  .config(["$stateProvider", "firebase", RoutingFunction])
-  .controller("WdiradioController", [WdiradiosControllerFunction])
+  .config(["$stateProvider", RoutingFunction])
+
+  .controller("SongsController", [
+    "$firebaseArray",
+    SongsControllerFunction])
+
+    function RoutingFunction($stateProvider){
+
+      $stateProvider
+        .state("Opening", {
+          url: "",
+          templateUrl: "js/ng-views/index.html"
+        })
+        .state("Index", {
+          url: "/songs",
+          templateUrl: "js/ng-views/songsindex.html",
+          controller: "SongsController",
+          controllerAs: "vm"
+        })
+    }
 
 
-function RoutingFunction($stateProvider){
-
-  $stateProvider
-    .state("wdiradiosIndex",{
-      url: "/",
-      templateUrl: "wdiradios-index.html",
-      controller: "WdiradioController",
-      controllerAs: "vm"
-    })
+function SongsControllerFunction($firebaseArray){
+  let ref = firebase.database().ref().child("songs");
+  this.songs = $firebaseArray(ref);
 }
