634147a4b69134200481536abcec8ea9a0ffaa62
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-09 13:50:49 -0500

    rolled back changes; too many rabbit holes

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,13 @@
 angular
   .module("wdiRadioApp", [
-    "ui-router",
     "firebase"
   ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ])
-  .controller("wdiRadioController", [
+  .controller("WdiRadioController", [
     "$firebaseArray",
     WdiRadioControllerFunction
   ])
 
-function RouterFunction($stateProvider){
-  console.log('Router setup correctly')
-  .state("songs", {
-    url: "/:songs",
-    controller: "wdiRadioController",
-    controllerAs: "vm"
-  });
-}
-
-function wdiRadioControllerFunction($firebaseArray){
+function WdiRadioControllerFunction($firebaseArray){
   let ref = firebase.database().ref().child("songs");
   this.songs = $firebaseArray(ref);
 }
