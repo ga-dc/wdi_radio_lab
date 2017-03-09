df41b1752669f7c9467d041bf8112980dd4be2d7
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-03-09 15:48:46 -0500

    added functions to app.js file

--- public/js/app.js
+++ public/js/app.js
@@ -7,83 +7,26 @@
       .controller("RootController", [RootControllerFunction])
       .controller("WDIRadioIndexController", ["$firebaseArray", WDIRadioControllerFunction])
 
-  }
+      function Router($stateProvider){
+        .state("root", {
+          url: "/",
+          templateUrl: "js/ng-views/root.html",
+          controller: "RootController"
+          controllerAs: "vm"
+        })
+        .state("wdiradioIndex", {
+          url: "/songs",
+          templateUrl: "js/ng-views/index.html",
+          controller: "WDIRadioIndexController",
+          controllerAs: "vm"
+        });
+      }
 
-// create templates for these routes
+      function RootControllerFunction() {
+        console.log("This the root page fam!")
+      }
 
-  // .controller("IGIndexController", [
-  //   "IGFactory",
-  //   "$state",
-  //   IGNewControllerFunction
-  // ])
-  // .controller("IGShowController",[
-  //   "IGFactory",
-  //   "$stateParams",
-  //   "$state",
-  //   IGEditControllerFunction
-  // ])
-  //
-  // function RouterFunction($stateProvider){
-  //   $stateProvider
-  //   .state("igIndex", {
-  //     url: "/ig",
-  //     templateUrl: "js/ng-views/index.html",
-  //     controller: "IGIndexController",
-  //     controllerAs: "vm"
-  //   })
-  //   .state("igNew", {
-  //     url: "/ig/new",
-  //     templateUrl: "js/ng-views/new.html",
-  //     controller: "IGNewController",
-  //     controllerAs: "vm"
-  //   })
-  //   .state("igShow", {
-  //     url: "/ig/:id",
-  //     templateUrl: "js/ng-views/show.html",
-  //     controller: "IGShowController"
-  //     controllerAs: "vm"
-  //   })
-  //   .state("igEdit", {
-  //     url: "/ig/:id/edit",
-  //     templateUrl: "js/ng-views/edit.html",
-  //     controller: "IGEditController",
-  //     controllerAs: "vm"
-  //   })
-  // }
-  //
-  // function IGFactoryFunction($resource){
-  //   return $resource("http://localhost:3000/entries/:id",{}, {
-  //     update: {method:"PUT"}
-  //   })
-  // }
-  //
-  // function IGIndexControllerFunction(IGFactory) {
-  //   this.grams = IGFactory.query();
-  // }
-  //
-  // function IGNewControllerFunction(IGFactory, $state){
-  //   this.ig = new IGFactory();
-  //   this.create = function(){
-  //     this.ig.$save(function(gram) {
-  //       $state.go("igShow", {id: ig.id})
-  //     })
-  //   }
-  // }
-  //
-  // function IGShowControllerFunction(IGFactory, $stateParams) {
-  //   this.ig = IGFactory.get({id: $stateParams.id});
-  // }
-  //
-  // function IGEditControllerFunction(IGFactory, $stateParams, $state) {
-  //   this.ig = IGFactory.get({id: $stateParams.id});
-  //   this.update = function(){
-  //     this.ig.$update({id: $stateParams.id}, function(ig) {
-  //       $state.go("igShow", {id: ig.id})
-  //     })
-  //   }
-  //   this.destroy = function(){
-  //     this.ig.$delete({id: $stateParams.id}, function(){
-  //       $state.go("igIndex")
-  //     })
-  //   }
-  // }
+      function WDIRadioIndexControllerFunction ($firebaseArray){
+        console.log("You found the index page")
+      }
+    }(());
