67103a864c9102b5d4bd4bfda7e7e19dd5fa6ab5
Erica Norby <ericanorby@gmail.com>
2017-03-09 10:21:40 -0500

    bootstrapped angular

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+angular
+  .module("wdiRadio", [
+    "firebase",
+    "ui-router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+
+function RouterFunction($stateProvider){
+
+}
