ba36b3ce3f54b553c1f3ec13983de56cdf0fafa8
Caroline Chen <carolinecaroline@gmail.com>
2017-05-09 15:33:25 -0400

    complete rails setup

--- wdi_radio_api/db/seeds.rb
+++ wdi_radio_api/db/seeds.rb
@@ -1,12 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
 
 
 Song.create([
-	{title: 'test', artist: 'test', album: 'test', cover_url: 'test.jpg'}
+	{title: "Burn the Witch", artist: "Radiohead", album: "Moon Shaped Pool", cover_url: "http://www.amoonshapedpool.com/imgof/cd.jpg"},
+	{title: "High", artist: "Sir Sly", album: "Single", cover_url: "https://i1.wp.com/atwoodmagazine.com/wp-content/uploads/2017/03/High-Sir-Sly.jpg"},
+	{title: "House of Gold", artist: "TwentyOne Pilots", album: "Vessel", cover_url: "https://upload.wikimedia.org/wikipedia/en/2/20/Vessel_by_Twenty_One_Pilots.jpg"},
+	{title: "The Chauffeur", artist: "Deftones", album: "B-Sides & Rarities", cover_url: "https://upload.wikimedia.org/wikipedia/en/3/35/Deftones_-_b-sides.jpg"},
+	{title: "Coin-operated Boy", artist: "The Dresden Dolls", album: "The Dresden Dolls", cover_url: "http://cdn3.pitchfork.com/albums/2432/dce05469.jpg"},
+	{title: "Everlong", artist: "Foo Fighters", album: "The Colour and the Shape", cover_url: "https://upload.wikimedia.org/wikipedia/en/0/0d/FooFighters-TheColourAndTheShape.jpg"}
 ])
