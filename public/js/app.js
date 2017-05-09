14a31b074e3de7cf28038b814ba1c3ddc09e25a8
Jeong Hoon Koo <junghoone17@gmail.com>
2017-05-09 16:34:39 -0400

    completed initializing a router

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,36 @@
-angular.module("radioApp", [])
+"use strict";
+
+angular
+.module("radioApp", ["ui.router"])
+.config(["$stateProvider", RouterFunction])
+.controller("SongIndexController", [
+  SongIndexControllerFunction
+])
+.controller("SongShowController", [
+  SongShowControllerFunction
+])
+
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("songIndex", {
+    url: "/",
+    controller: "SongIndexController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/index.html"
+    })
+  .state("songShow", {
+    url: "/songs",
+    controller: "SongShowController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/show.html"
+    })
+}
+
+function SongIndexControllerFunction() {
+  console.log("index!")
+}
+
+function  SongShowControllerFunction() {
+  console.log("show")
+}
