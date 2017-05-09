83f93f3c530a6c55659c2a6133e597d22765d353
Caroline Chen <carolinecaroline@gmail.com>
2017-05-09 15:06:29 -0400

    add test seed

--- wdi_radio_api/db/seeds.rb
+++ wdi_radio_api/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Song.create([
+	{title: 'test', artist: 'test', album: 'test', cover_url: 'test.jpg'}
+])
