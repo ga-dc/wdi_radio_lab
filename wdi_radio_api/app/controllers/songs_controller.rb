1cfbbc9ed9d8e473c045bcfca3e0ccadcc0fdc46
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-09 15:43:55 -0400

    set up routes and json access

--- wdi_radio_api/app/controllers/songs_controller.rb
+++ wdi_radio_api/app/controllers/songs_controller.rb
@@ -1,5 +1,11 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @songs }
+    end
   end
+
 end
