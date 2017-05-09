e2b0ace8594d0206a7dbefb95260c994feb2dad7
Caroline Chen <carolinecaroline@gmail.com>
2017-05-09 16:24:11 -0400

    initialize a router

--- wdi_radio_angular/js/app.js
+++ wdi_radio_angular/js/app.js
@@ -1,2 +1,34 @@
 angular
-	.module("wdi_radio", ["ui.router"])
+	.module("radio", ["ui.router"])
+	.config(["$stateProvider", Router])
+	.controller("RadioWelcomeController", [
+		RadioWelcomeControllerFunction
+		])
+	.controller("RadioIndexController", [
+		RadioIndexControllerFunction
+		])
+
+
+	function Router($stateProvider){
+		$stateProvider
+		.state("radioWelcome", {
+			url: "/", 
+			controller: "RadioWelcomeController",
+			controllerAs: "vm",
+			templateUrl: "js/ng-views/welcome.html"
+		})
+		.state("radioIndex", {
+			url: "/songs",
+			controller: "RadioIndexController",
+			controllerAs: "vm",
+			templateUrl: "js/ng-views/index.html"
+		})
+	}
+
+	function RadioWelcomeControllerFunction(){
+		console.log("I'm in the welcome controller!")
+	}
+
+	function RadioIndexControllerFunction(){
+		console.log("and I'm in the index controller!")
+	}
