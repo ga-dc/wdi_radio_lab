715f058c30dda0585823a87eb4a5848928c16072
meagdoh <meagdoherty@gmail.com>
2017-05-09 18:14:00 -0400

    Resource setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,37 @@
-angular.module("wdiRadioApp", [])
+
+angular
+.module("wdiRadioApp", [
+  "ui.router",
+  "ngResource"
+])
+.controller("SongIndexController",[
+  "SongFactory",
+  SongIndexControllerFunction
+])
+.config(["$stateProvider", RouterFunction])
+.factory("SongFactory",[
+  "$resource",
+  SongFactoryFunction
+]);
+
+function SongFactoryFunction($resource){
+  return $resource ("http://localhost:3000/songs/:id");
+}
+
+function SongIndexControllerFunction (SongFactory){
+  this.songs = SongFactory.query();
+}
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("songIndex",
+    {url: "/songs",
+    templateUrl: "js/ng-views/index.html",
+    controller: "SongIndexController",
+    controllerAs: "vm"
+    })
+  .state("songShow",
+    {url: "/songs/:id",
+    templateUrl: "js/ng-views/show.html",
+    });
+}
--- wdi_radio_api/app/controllers/songs_controller.rb
+++ wdi_radio_api/app/controllers/songs_controller.rb
@@ -1,6 +1,13 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
-    render json: @songs.to_json, status: :ok
+    render json: @songs, status: :ok
+  end
+  def show
+    @song = Song.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @song}
+    end
   end
 end
