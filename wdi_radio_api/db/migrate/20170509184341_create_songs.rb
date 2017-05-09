65665ae9a65d2b8e20f891d5379a07aa7e876b79
Curtis Ostle <curtisostle@gmail.com>
2017-05-09 15:01:00 -0400

    finished initial seeds

---
+++ wdi_radio_api/db/migrate/20170509184341_create_songs.rb
@@ -0,0 +1,10 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :artist
+      t.string :album
+      t.string :album_art
+    end
+  end
+end
