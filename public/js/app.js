33279895fa0881e09e831881eeb63e1b7e2a6b46
Whitney <whitney.purdum@gmail.com>
2017-05-09 17:52:48 -0400

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,49 @@
 angular
   .module("wdiradio", ["ui.router", "ngResource"])
   .config(["$stateProvider", RouterFunction])
+  .factory("SongFactory", [
+    "$resource",
+    SongFactoryFunction
+  ])
+  .controller("SongsIndexController", [
+    "SongFactory",
+    SongsIndexControllerFunction
+    ])
+  .controller("SongShowController", [
+    "SongFactory",
+    "$stateParams",
+    SongShowControllerFunction
+  ])
+
+  // functions
 
   function RouterFunction($stateProvider){
     $stateProvider
-    .state("SongsIndexControllerIndex", {
+    .state("SongsIndexControllerFunction", {
       url: "/songs",
-      templateUrl: "js/ng-views/index.html"
+      templateUrl: "js/ng-views/index.html",
       controller: "SongsIndexController",
+      controllerAs: "vm"
+    }).state("songShow", {
+      url: "/songs/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "SongShowController",
       controllerAs: "vm"
     })
   }
+
+  function SongFactoryFunction($resource){
+    return $resource("http://localhost:3000/songs/:id.json");
+  }
+
+  function SongsIndexControllerFunction ( SongFactory ){
+    this.songs = SongFactory.query();
+  }
+
+  function SongShowControllerFunction(SongFactory, $stateParams) {
+    this.song = SongFactory.get({id: $stateParams.id});
+  }
+
+  // look up ng resource
+
+// controllers
--- wdi_radio_api/app/controllers/songs_controller.rb
+++ wdi_radio_api/app/controllers/songs_controller.rb
@@ -1,3 +1,18 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @songs}
+    end
 
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @song}
+    end
+  end
 end
