471698edb4258c02a44159705fe08c656cd569c7
Andres Arango <andresarangov17@gmail.com>
2017-05-10 19:12:23 -0400

    rails setup. angular setup

---
+++ wdi_radio_api/app/controllers/songs_controller.rb
@@ -0,0 +1,19 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @songs}
+    end
+  end
+
+  def show
+    @song = Song.find(params[:id])
+
+    respond_to do |format|
+      format.html {render :show}
+      format.jason {render json: @song}
+    end
+  end
+end
