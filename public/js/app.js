c65ace111b8a209180297dfe6cea00746f039605
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-10 11:11:43 -0400

    working on Angular and resource

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,54 @@
+"use strict";
+
 angular
   .module("radioapp", [
-    "ui.router"
+    "ui.router",
+    "ngResource"
   ])
-  .
+  .config([
+    "stateProvider",
+    RouterFunction
+  ])
+  .factory("SongFactory", [
+    "resource",
+    SongFactoryFunction
+  ])
+  .controller("SongIndexController", [
+    "SongFactory",
+    SongIndexControllerFunction
+  ])
+  .controller("SongShowController", [
+    "$stateParams",
+    "SongFactory",
+    SongShowControllerFunction
+  ])
+
+
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("songIndex", {
+    url: "/",
+    templateUrl: "js/ng-views/index.html",
+    controller: "SongIndexController",
+    controllerAs: "vm"
+  })
+  .state("songShow", {
+    url: "/songs/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "SongShowController",
+    controllerAs: "vm"
+  })
+}
+
+function SongFactoryFunction($resource){
+  return $resource("http://localhost:3000/songs/:id.json")
+}
+
+function SongIndexControllerFunction() {
+  this.songs = SongFactory.query()
+}
+
+function SongShowControllerFunction() {
+  this.song = SongFactory.get({id: $stateParams.id})
+}
