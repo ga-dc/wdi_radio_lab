d881b3308923d430b0787f22da123c25ecb2e66d
Curtis Ostle <curtisostle@gmail.com>
2017-05-10 10:58:11 -0400

    creating views

--- wdi_radio_angular/js/app.js
+++ wdi_radio_angular/js/app.js
@@ -15,6 +15,16 @@ angular
     "SongFactory",
     SongIndexControllerFunction
   ])
+  .controller("SongNewController", [
+    "SongFactory",
+    SongNewControllerFunction
+  ])
+  .controller("SongEditController", [
+    "SongFactory",
+    "$stateParams",
+    SongEditControllerFunction
+  ]);
+
 
 
   function RouterFunction($stateProvider){
@@ -25,11 +35,40 @@ angular
       controller: "SongIndexController",
       controllerAs: "vm"
     })
-
-    function SongIndexControllerFunction(SongFactory){
-      this.songs = SongsFactory.query();
+    .state("songNew", {
+      url:"/songs/new",
+      templateUrl:"js/ng-views/new.html",
+      controller:"SongNewController",
+      controllerAs:"vm"
+    })
+    .state("songEdit", {
+      url:"/songs/:id/edit",
+      templateUrl:"js/ng-views/edit.html",
+      controller:"SongEditController",
+      controllerAs:"vm"
+    })
+  }
+    function SongFactoryFunction($resource){
+      return $resource( "http://localhost:3000/songs.json", {}, {});
     }
 
-    function SongFactoryFunction( $resource ){
-      return $resource( "http://localhost:3000/songs/", {}, {});
-  }
+    function SongIndexControllerFunction(SongFactory){
+      this.songs = SongFactory.query();
+    }
+
+    function SongEditControllerFunction(SongFactory, $stateParams) {
+      this.song = SongFactory.get({id: $stateParams.id});
+      this.update = function(){
+      this.song.$update({id: $stateParams.id});
+      }
+      this.destroy = function(){
+        this.grumble.$delete({id: $stateParams.id});
+      }
+    }
+
+    function SongNewControllerFunction(SongFactory) {
+      this.song = new SongFactory();
+      this.create = function(){
+        this.song.$save();
+      }
+    }
