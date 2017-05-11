8b5ea3e95c655821e71021abd1ae4b6850cd42ea
amlak befekadu <amlakgirma@yahoo.com>
2017-05-10 20:00:14 -0400

    API AND ANGULAR

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,29 @@
 angular
 .module("crazyApp", [
-  
+  "ui.router",
+  "ngResource"
 ])
+.config([$stateProvider,RouterFunction])
+.factory("songfactory", "$resource",[songfactoryfunction])
+.controller("songsshowcontroller",[songsshowcontrollerfunction])
+function RouterFunction($stateProvider) {
+  $stateProvider
+  .state('index',{
+    url: '/',
+    controller: "songsController",
+    controllerAs: 'vm'
+
+  })
+  .state('show',{
+    url:'/songs',
+    controller: "songsshowcontroller",
+    controllerAs: 'vm',
+    templeteUrl: "js/"
+  })
+}
+function songfactoryfunction($resource){
+  return $resource("http://localhost:3000/songs")
+}
+function songsshowcontrollerfunction(){
+  this.songs = songfactory.query()
+}
