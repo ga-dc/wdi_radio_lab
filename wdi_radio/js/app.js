59b963cf9d7cd9b51dbf4aa8702b157dda2f2fd6
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-21 12:51:24 -0400

    complete

--- wdi_radio/js/app.js
+++ wdi_radio/js/app.js
@@ -1,3 +1,31 @@
 angular
-.module("wdiradio", [])
-.controller("wdiradioIndexController", wdiradioIndexControllerFunction)
+.module("wdiradio", ["ui.router", "ngResource"])
+.config(["$stateProvider", RouterFunction])
+.controller("wdiradioIndexController", ["wdiRadioFactory", wdiradioIndexControllerFunction])
+.factory("wdiRadioFactory",["$resource", FactoryFunction])
+
+
+function RouterFunction($stateProvider) {
+  $stateProvider
+  .state("wdiRadioIndex", {
+    url: "/",
+    templateUrl: "js/ng-views/index.html",
+    controller: "wdiradioIndexController",
+    controllerAs: "vm"
+  })
+  .state("wdiRadioSongs", {
+    url: "/songs",
+    templateUrl: "js/ng-views/songs.html",
+    controller: "wdiradioIndexController",
+    controllerAs: "vm"
+  })
+
+}
+
+function wdiradioIndexControllerFunction( wdiRadioFactory) {
+  this.songs = wdiRadioFactory.query()
+}
+
+function FactoryFunction( $resource ){
+  return $resource( "http://localhost:3000/songs.json");
+}
