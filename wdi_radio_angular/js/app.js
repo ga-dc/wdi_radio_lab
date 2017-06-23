5a3d6715ff1d1a3ea9de1ff1766901dbc36d1189
Tarik Kazanci <tarikkzc@gmail.com>
2017-06-22 21:32:47 -0400

    full crud songs

--- wdi_radio_angular/js/app.js
+++ wdi_radio_angular/js/app.js
@@ -9,6 +9,7 @@ angular
   "ui.router",
   "ngResource"
 ])
+
 .config(["$stateProvider", RouterFunction])
 
 .factory( "SongsFactory", [
@@ -16,11 +17,24 @@ angular
   SongsFactoryFunction
 ])
 
-
 .controller("SongsIndexController", [
     "SongsFactory",
+    "$state",
     SongsIndexControllerFunction
-  ]);
+  ])
+
+.controller("SongsShowController", [
+      "SongsFactory",
+      "$stateParams",
+      SongsShowControllerFunction
+  ])
+
+.controller("SongsEditController", [
+        "SongsFactory",
+        "$stateParams",
+        "$state",
+        SongsEditControllerFunction
+ ]);
 
   function RouterFunction($stateProvider){
     $stateProvider
@@ -36,14 +50,57 @@ angular
       controller: "SongsIndexController",
       controllerAs: "vm",
       templateUrl: "js/songs-views/index.html"
+    })
+
+
+    .state("songShow", {
+      url: "/songs/:id",
+      controller: "SongsShowController",
+      controllerAs: "vm",
+      templateUrl: "js/songs-views/show.html"
+    })
+
+    .state("songEdit", {
+      url: "/songs/:id/edit",
+      controller: "SongsEditController",
+      controllerAs: "vm",
+      templateUrl: "js/songs-views/edit.html"
     });
   }
 
-  function SongsIndexControllerFunction (SongsFactory) {
+  function SongsIndexControllerFunction (SongsFactory, $state) {
     this.songs = SongsFactory.query();
+
+    // for new song
+    this.song = new SongsFactory();
+    this.create = function() {
+      this.song.$save().then( function(){
+        $state.go("songsIndex")
+      })
+    }
   }
 
+  function SongsShowControllerFunction (SongsFactory, $stateParams) {
+    this.song = SongsFactory.get({id: $stateParams.id});
+  }
+
+  function SongsEditControllerFunction (SongsFactory, $stateParams, $state) {
+    this.song = SongsFactory.get({id: $stateParams.id});
+    this.update = function() {
+      this.song.$update({id: $stateParams.id}).then( function(){
+        $state.go("songsIndex")
+      })
+    }
+    this.destroy = function() {
+      this.song.$delete({id: $stateParams.id}).then( function(){
+        $state.go("songsIndex")
+      })
+    }
+  }
 
   function SongsFactoryFunction($resource) {
-    return $resource ("http://localhost:3000/songs/:id.json");
+    return $resource ("http://localhost:3000/songs/:id.json", {}, {
+        update: {method: "PUT"}
+
+    });
   }
--- wdi_radio_api/config/application.rb
+++ wdi_radio_api/config/application.rb
@@ -8,13 +8,19 @@ Bundler.require(*Rails.groups)
 
 module WdiRadioApi
   class Application < Rails::Application
+
+    # headers['Access-Control-Allow-Origin'] = '*'
+    # headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    # headers['Access-Control-Request-Method'] = '*'
+    # headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     config.middleware.insert_before 0, Rack::Cors do
          allow do
            origins '*'
-           resource '*', :headers => :any, :methods => [:get, :post, :options]
+           resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
          end
        end
 
