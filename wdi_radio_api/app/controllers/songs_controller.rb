6737cc9e228e8a8bec15645c800316bbeeeb2299
Scott Pierce <scottpiercew@gmail.com>
2017-06-23 08:20:46 -0400

    completed rails_api and angular

--- wdi_radio_api/app/controllers/songs_controller.rb
+++ wdi_radio_api/app/controllers/songs_controller.rb
@@ -1,16 +1,11 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
-    respond_to do |format|
-      format.html { render :index }
-      format.json { render json: @songs }
-    end
+      render json: @songs
   end
 
   def show
     @song = Song.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @song }
+      render json: @song
   end
 end
--- wdi_radio_api/config/routes.rb
+++ wdi_radio_api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'songs/index'
+  resources :songs
 
-  get 'songs/show'
+  # get 'songs/index'
+  #
+  # get 'songs/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
